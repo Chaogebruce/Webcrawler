d7b853a2dd3c27daf2cfb10b1ad0ad317b46fbc3
Chaogebruce <romancavalrycc@gmail.coom>
2016-03-29 17:37:26 +0800

    begin use 500 method

--- app.py
+++ app.py
@@ -4,21 +4,84 @@
 __author__ = 'Bruce Chen'
 
 import socket
+import logging
 from bs4 import BeautifulSoup
-def fetch(url):
-    sock = socket.socket()
-    sock.connect(('guazi.com',80))
-    request = 'GET {} HTTP/1.1\r\nHost: www.guazi.com\r\n\r\n'.format(url)
-    sock.send(request.encode('ascii'))
-    response = b''
-    chunk = sock.recv(4096)
-    while chunk:
-        response += chunk
-        chunk = sock.recv(4096)
-    soup = BeautifulSoup(response, 'lxml')
-    t= ''
-    for link in soup.findAll('a'):
-        t += str(link.get('href')) + '\r\n'
-    print(t)
+from selectors import DefaultSelector, EVENT_WRITE, EVENT_READ
 
-fetch('/sh/')
+logging.basicConfig(level=logging.INFO)
+
+selector = DefaultSelector()
+
+sock = socket.socket()
+sock.setblocking(False)
+try:
+    sock.connect(('guazi.com',80))
+except BlockingIOError:
+    pass
+
+def connected():
+    selector.unregister(sock.fileno())
+    logging.info('connected!')
+
+def loop():
+    while True:
+        events = selector.select()
+        for event_key, event_mask in events:
+            callback = event_key.data
+            callback()
+
+urls_todo = {'/'}
+seen_urls = {'/'}
+
+class Fetcher:
+
+
+    def __init__(self,url):
+        self.response = b''
+        self.url = url
+        self.sock =  None
+
+    #Method on Fetcher class
+    def fetch(self):
+        self.sock = socket.socket()
+        self.sock.setblocking(False)
+        try:
+            self.sock.connect(('guazi.com',80))
+        except BlockingIOError:
+            pass
+
+        # Register next callback.
+        selector.register(self.sock.fileno(),EVENT_WRITE,self.connected)
+
+    def connected(self, key, mask):
+        logging.info('connected!')
+        selector.unregister(key.fd)
+        request = 'GET {} HTTP/1.1\r\nHost: www.guazi.com\r\n\r\n'.format(url)
+        self.sock.send(request.encode('ascii'))
+
+        # Register next callback.
+        selector.register(self.sock.fileno(),EVENT_READ,self.read_response)
+
+    def read_response(self, key, mask):
+        global stopped
+
+        chunk = self.sock.recv(4096)
+        if chunk:
+            self.response += chunk
+        else:
+            selector.unregister(key.fd) # Done reading
+            links = self.parse_links()
+
+    def parse_links(self):
+        links = set()
+
+def parse_links(html):
+    soup = BeautifulSoup(html, 'lxml')
+    return soup.findAll('a').get('href')
+
+with open('guazi.txt','rt',encoding='utf-8') as f:
+    html = f.read()
+soup = BeautifulSoup(html,'lxml')
+with open('links.txt','w') as f:
+    for link in soup.findAll('a'):
+        f.write('%s\n' % link.get('href'))
